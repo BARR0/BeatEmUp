b058b1ce2d267d92a01a61eaa492a4748cf0465f
geraJuarez <geratrex781@gmail.com>
2017-02-21 16:59:49 -0600

    invisible walls and colliders

---
+++ Assets/Proyectiles/Proyectil.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Proyectil : MonoBehaviour {
+	private Rigidbody rb;
+	public int force;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rb.AddRelativeForce (force, 0, 0, ForceMode.Impulse);
+	}
+}
