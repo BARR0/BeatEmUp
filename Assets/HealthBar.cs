d0e798cca4a084d8d660e9c1f118a98c0c51f05a
José Carlos Peñuelas Armenta <jose18_carl@hotmail.com>
2017-04-02 22:35:44 -0500

    Subsolution a HealthBar

--- Assets/HealthBar.cs
+++ Assets/HealthBar.cs
@@ -11,17 +11,22 @@ public class HealthBar : MonoBehaviour {
     private Text text;
     // Use this for initialization
     void Start() {
+        if(this.number == "1")
+        {
+            this.number = "";
+        }
         Transform sliderTransform = transform.GetChild(0);
         slider = sliderTransform.GetComponent<Slider>();
-
+        player = null;
         Transform textTransform = transform.GetChild(1);
         text = textTransform.GetComponent<Text>();
 
-        foreach (PlayerController pc in GameController.players) {
-            Debug.Log("Input Axis " + pc.inputAxis);
-            if (pc.inputAxis.Equals(number))
+        foreach (PlayerController pc in GameController.players) {   
+            if (pc.inputAxis == number  || pc.inputAxis == "")
             {
+                Debug.Log("Input Axis " + pc.inputAxis + " pcName: " + pc.gameObject.name);
                 player = pc;
+                break;
             }
         }
         if (player == null)
