801264a6dc8feffa3aa1d2d1130205a02e32931a
geraJuarez <geratrex781@gmail.com>
2017-04-29 17:49:31 -0600

    LevelUpAssetSprite and main screen at first screen shown

---
+++ Assets/PrefabsAvatar/_LevelUp/LvlupDestroyer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LvlupDestroyer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void destroyAngel() {
+
+		Destroy (this.gameObject);
+	}
+}
--- Assets/UICharSelect/MainScreen/MenuLogic.cs
+++ Assets/UICharSelect/MainScreen/MenuLogic.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuLogic : MonoBehaviour {
 
@@ -11,11 +12,16 @@ public class MenuLogic : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		if (Input.GetAxis ("Fire1") != 0) {
+
+			SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+		}
+
 	}
 
 	public void Startgame() {
 
-		print ("hola");
+		SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
 	}
 }
