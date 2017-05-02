103ff54d1d1fb75227ac0504d732aee2b1d7758b
Andres B <andresbarroe@gmail.com>
2017-05-02 01:39:40 -0500

    Game Over Screen

---
+++ Assets/GameOver.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (Reset());
+	}
+	
+	IEnumerator Reset(){
+		yield return new WaitForSeconds(10);
+		SceneManager.LoadScene ("MainMenu");
+	}
+}
