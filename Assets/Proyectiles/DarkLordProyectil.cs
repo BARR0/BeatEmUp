fd05131f44b4f6d058fe444686d495f2e6c6a0f1
José Carlos Peñuelas Armenta <jose18_carl@hotmail.com>
2017-05-08 21:46:38 -0500

    Agregado un sprite para el ataque de DarkLord y animaciones de los nuevos enemigos mejoradas

---
+++ Assets/Proyectiles/DarkLordProyectil.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DarkLordProyectil : MonoBehaviour {
+	private Rigidbody rb;
+	public float force;
+
+	// Use this for initialization
+	void Start () {
+        Destroy(gameObject, 10);
+	}
+
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(-5*Time.deltaTime*force,0,0);
+	}
+
+	void OnBecameInvisible() {
+		Destroy (this.gameObject);
+	}
+
+    void OnTriggerEnter(Collider c)
+    {
+        if(c.gameObject.layer == 8)
+        {
+            PlayerController pc = c.GetComponent<PlayerController>();
+            pc.life -= 10;
+        }
+    }
+}
